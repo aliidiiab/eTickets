8c0ae9c851d921ed4dc34bce48a0f0e21303226a
aliidiiab <ali-diab97@hotmail.com>
2022-04-17 00:38:53 +0200

    Repos

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext context;
+        public EntityBaseRepository(AppDbContext _appDbContext)
+        {
+            context = _appDbContext;
+        }
+        public async Task AddAsync(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+        
+
+        public async Task DeleteAsync(int id)
+        {
+            var newentity = await GetByIdAsync(id);
+            EntityEntry entityEntry = context.Entry<T>(newentity);
+            entityEntry.State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()=> await context.Set<T>().ToListAsync();
+        
+
+        public async Task<T> GetByIdAsync(int id)=> await context.Set<T>().FirstOrDefaultAsync(i => i.Id == id);
+        
+
+        public async Task UpdateAsync(int id, T newentity)
+        {
+            EntityEntry entityEntry = context.Entry<T>(newentity);
+            entityEntry.State=EntityState.Modified;
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -1,4 +1,5 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,41 +7,14 @@ using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public class ActorsService : IActorsService
+    public class ActorsService :EntityBaseRepository<Actor>,IActorsService
     {
-        private readonly AppDbContext context;
-        public ActorsService(AppDbContext _context)
+        public ActorsService(AppDbContext context):base(context)
         {
-            context = _context;
+            
         }
-        public async Task AddAsync(Actor actor)
-        {
-            await context.Actors.AddAsync(actor);
-            await context.SaveChangesAsync();
-        }
+        
+       
 
-        public async Task DeleteAsync(int id)
-        {        
-            context.Actors.Remove(await GetByIdAsync(id));
-            await context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            return await context.Actors.ToListAsync();  
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var result =await context.Actors.FirstOrDefaultAsync(i=>i.Id == id);
-            return result;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
-        {
-            context.Update(newActor);
-            await context.SaveChangesAsync();
-            return newActor;
-        }
     }
 }
