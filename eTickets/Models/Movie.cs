498a6f5a81b6d83a098dc9309a182085524be9d3
aliidiiab <ali-diab97@hotmail.com>
2022-04-16 05:38:24 +0200

    IntitalCatalogV1

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using eTickets.Data;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Descreption { get; set; }
+
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+        public List<Actor_Movie> Actors_Movies { get; set; }
+        
+        public int CinemaID { get; set; }
+        [ForeignKey("CinemaID")]
+        public Cinema Cinema   { get; set; }
+
+        public int ProducerID { get; set; }
+        [ForeignKey("ProducerID")]
+        public Producer Producer { get; set; }
+
+
+    }
+}
