f8edd40d4d9a2c6e2b1c536bfd6241572caea27e
aliidiiab <ali-diab97@hotmail.com>
2022-04-16 23:53:47 +0200

    Actor

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -1,21 +1,68 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace eTickets.Controllers
 {
     public class ActorsController : Controller
     {
-        private readonly AppDbContext context;
-        public ActorsController(AppDbContext _context)
+        private readonly IActorsService service;
+        public ActorsController(IActorsService _service)
         {
-            context = _context;
+            service = _service;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var data = context.Actors.ToList();
-            return View(data);
+            return View(await service.GetAllAsync());
         }
+        public IActionResult Create()
+        {
+            return View(new Actor());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await service.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Actor actordetails=await service.GetByIdAsync(id);
+            if (actordetails == null) return View("NotFound");
+            return View(actordetails);
+            
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            Actor actordetails = await service.GetByIdAsync(id);
+            if (actordetails == null) return View("NotFound");
+            return View(actordetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,[Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await service.UpdateAsync(id,actor);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            Actor actordetails = await service.GetByIdAsync(id);
+            if (actordetails == null) return View("NotFound");
+            await service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+        
     }
 }
---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,46 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext context;
+        public ActorsService(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public async Task AddAsync(Actor actor)
+        {
+            await context.Actors.AddAsync(actor);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {        
+            context.Actors.Remove(await GetByIdAsync(id));
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAllAsync()
+        {
+            return await context.Actors.ToListAsync();  
+        }
+
+        public async Task<Actor> GetByIdAsync(int id)
+        {
+            var result =await context.Actors.FirstOrDefaultAsync(i=>i.Id == id);
+            return result;
+        }
+
+        public async Task<Actor> UpdateAsync(int id, Actor newActor)
+        {
+            context.Update(newActor);
+            await context.SaveChangesAsync();
+            return newActor;
+        }
+    }
+}
